6fcf1b09ba12fa81e59f73f0bbb251dc9cfe3939
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-09-04 12:14:47 +0100

    fix white space registration

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "36a039dba88ed689390eb019f0b2c252",
-"index.html": "36c80058c972b45b6a26cd87d416004a",
-"/": "36c80058c972b45b6a26cd87d416004a",
-"main.dart.js": "3c591714b932812436bbe02b960e0891",
+"index.html": "8e5dabf15d43c519c2c683307f8f79b8",
+"/": "8e5dabf15d43c519c2c683307f8f79b8",
+"main.dart.js": "95375e46e2bc42803695032ecf2494bb",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "f23971949463ec869f4e1ade99ee31cb",
 "icons/Icon-192.png": "f23971949463ec869f4e1ade99ee31cb",
