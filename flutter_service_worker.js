90debf30897a2b7a715ca312633c2b1e51adca25
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-09-04 23:05:57 +0100

    add room dynamic

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "36a039dba88ed689390eb019f0b2c252",
-"index.html": "603fd854a59e70551f094302d81c7abf",
-"/": "603fd854a59e70551f094302d81c7abf",
-"main.dart.js": "05a3193a9daab1db261cf3dcd4d490d3",
+"index.html": "f07d525a8b2524848fbffc98b74aa641",
+"/": "f07d525a8b2524848fbffc98b74aa641",
+"main.dart.js": "ef2170de6bc9bc9069ef7dbf2bb21fe2",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "f23971949463ec869f4e1ade99ee31cb",
 "icons/Icon-192.png": "f23971949463ec869f4e1ade99ee31cb",
